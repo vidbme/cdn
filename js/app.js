0d47b9c733085346025fd3332db72f6412b27aa2
Kathrine Pearson <vidb@mail.com>
2018-01-10 13:04:53 +0000

    Make it better

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 requirejs.config({
-    "baseUrl": "js",
-    "paths": {
-      "jquery": "jquery",
-      "popper": "popper",
-      "app": "app",
-      "bootstrap": "bootstrap.min"
+    'appDir': '.',
+    'baseUrl': 'js',
+    'paths': {
+      'jquery': ['//cdn.vidb.me/js/jquery','https://code.jquery.com/jquery-3.2.1.min.js'],
+      'popper': ['//cdn.vidb.me/js/popper','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'],
+      'app': ['//cdn.vidb.me/js/app'],
+      'bootstrap': ['//cdn.vidb.me/js/bootstrap.min','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js']
     },
-    "shim": {
-        "bootstrap": {
-        	"deps": ["jquery","popper"],
-			"exports": "$"
+    'shim': {
+        'bootstrap': {
+        	'deps': ['jquery','popper'],
+        	'exports': '$'
         	 },
-        "main": { "deps": ["jquery","bootstrap"] }
+        'main': { 'deps': ['jquery','bootstrap'] }
     }
 });
  
-require(["popper"],function(p){
+require(['popper'],function(p){
   window.Popper = p;
-  require(["app"],function(app){
+  require(['app'],function(app){
     window.mainApp = app;
   });
 });
