11dbf9785e124f03b14b811415dda5a954ed1d0b
Kathrine Pearson <vidb@mail.com>
2018-01-28 22:24:27 +0000

    Make it better

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ requirejs.config({
 		popper: "//cdn.vidb.me/js/popper.min",
 		bootstrap: "//cdn.vidb.me/js/bootstrap.bundle.min",
 		fontawesome: "//cdn.vidb.me/js/fa-solid.min",
-		recaptcha: "https://www.google.com/recaptcha/api"
+		recaptcha: "https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
 	},
 	shim : {
 		bootstrap: ["jquery"]
@@ -17,6 +17,7 @@ require(["jquery"], function($) {
 	require(["bootstrap"], function(bootstrap) {
 		$('[data-toggle="tooltip"]').tooltip();
 		$('#pop-login').on('shown.bs.modal', function (e) {
+			require(["recaptcha"]);
 		});
 	});
 });
